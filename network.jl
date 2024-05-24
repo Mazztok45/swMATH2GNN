bb4038073e9e80c5f500bf5c5dcf65bd16a5c462
a.vonderhaar01 <a.vonderhaar01@stud.uni-goettingen.de>
2024-05-24 16:52:33 +0200

    treid out some statistics

--- network.jl
+++
@@ -1,3 +0,0 @@
-# Working more with the data, I got the idea that it might be helpful to
-# create a network graph with the "related software" information
-# to be continued
---
+++ software_relations_network.jl
@@ -0,0 +1,3 @@
+# Working more with the data, I got the idea that it might be helpful to
+# create a network graph with the "related software" information
+# to be continued
