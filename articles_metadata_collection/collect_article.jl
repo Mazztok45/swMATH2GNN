a50f58c6163a157d95df5b6fb5e247b53b127d30
Mazztok45 <maxence.azzouz@gmail.com>
2024-06-05 11:30:35 +0200

    adding code for extraction of articles' metadata associated with the software

--- articles_metadata_collection/collect_article.jl
+++ articles_metadata_collection/collect_article.jl
@@ -9,16 +9,16 @@ df = CSV.read("data/full_df.csv", DataFrame)
 df = subset(df, :articles_count => a -> a.>0)
 
 for id in df[!,"id"]
-    if isfile("articles_metadata_collection/"*string(id)*".json")==false
-
+    if isfile("articles_metadata_collection/"*string(id)*".json")==false || filesize("articles_metadata_collection/"*string(id)*".json")==0
+        
         println(id)
         try
             r = HTTP.request("GET", "https://api.zbmath.org/v1/document/_structured_search?page=0&results_per_page=100&software%20id="*string(id))
             println(r.status)
-            println(String(r.body))
+            #println(String(r.body))
             write("articles_metadata_collection/"*string(id)*".json",String(r.body)) 
         catch y
-            println("Exception: "*string(y)) 
+            println("Exception: "*string(id)) 
         end
     else
         "articles_metadata_collection/"*string(id)*".json already exists"
