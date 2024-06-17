73e15c57ecbd6d73b91c49b14439dc465ddde021
a.vonderhaar01 <a.vonderhaar01@stud.uni-goettingen.de>
2024-06-17 12:47:29 +0200

    extracted data into dicts

--- extract_article_metadata.jl
+++ extract_article_metadata.jl
@@ -26,29 +26,74 @@ function extract_articles_metadata()
                     end
                     push!(author_names, name)
                 end
-                df_dict["author_name"] = author_names
+                df_dict[:author_name] = author_names
             
             elseif key.first == :document_type
                 doc_type = key.second.description
-                df_dict["document_type"] = doc_type
+                df_dict[:document_type] = doc_type
             
             
             # to be implemted, very brute force but didn't find a better solution
             # because we don't need all the information in the nested dicts.
 
             elseif key.first == :editorial_contributions
+                text = key.second[1].text
+                df_dict[:text] = text
+                reviewer_name = key.second[1].reviewer.name
+                df_dict[:reviewer_name] = reviewer_name
+
             
             elseif key.first == :language
+                lang =  key.second.languages
+                df_dict["language"] = lang
             
             elseif key.first == :links
-            
+                if key.second != []
+                    ident = key.second[1].identifier
+                    if ident == nothing
+                        doi = nothing
+                    else
+                        doi = "https://doi.org/" * ident
+                    end
+                    df_dict[:doi] = doi
+                else
+                    df_dict[:doi] = nothing
+                end
+
             elseif key.first == :msc
+                df_dict[:msc] = nothing
             
             elseif key.first == :references
+                if key.second != []
+                    ref_dois = []
+                    for ref in key.second
+                        ref_doi = ref.doi
+                        if ref_doi == nothing
+                            continue                        
+                        elseif startswith(ref_doi, "zbMATH Open Web Interface contents unavailable")
+                            continue
+                        else
+                            ref = ref_doi
+                        end
+                        push!(ref_dois, ref)
+                    end
+                else
+                    ref_dois = nothing
+                end
+                df_dict[:ref_dois] = ref_dois
             
-            elseif key.first == :language
             
             elseif key.first == :title
+                title = key.second.title
+                if title != nothing && startswith(title, "zbMATH Open Web Interface contents unavailable")       
+                    title = "Not available"
+                end
+                df_dict[:title] = title
+                subtitle = key.second.subtitle
+                if subtitle != nothing && startswith(subtitle, "zbMATH Open Web Interface contents unavailable")
+                    subtitle = "Not available"
+                end
+                df_dict[:subtitle] = subtitle
             
             # else, all the keys that are not nested
             elseif in(key.first, selected_vars)
@@ -56,11 +101,19 @@ function extract_articles_metadata()
             end
             
         end
-        println(df_dict)
+        #println(df_dict)
         push!(df_list, df_dict)
     end
+    #df = DataFrame(df_list)
+    println(df_list)
+    return df_list
 end
 
+
+function dict_list_to_df(dict_list)
+    df = DataFrame()
+    return df
+end
 
         
     #     for k in keys(json_file)
