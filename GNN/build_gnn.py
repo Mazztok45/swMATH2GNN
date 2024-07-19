a7ed6cc146c735e2187719244a2591af7dd1a7b7
a.vonderhaar01 <a.vonderhaar01@stud.uni-goettingen.de>
2024-07-19 11:24:00 +0200

    tried other preprocessing  method for keywords

--- GNN/build_gnn.py
+++ GNN/build_gnn.py
@@ -1,16 +1,18 @@
 import pandas as pd
 #from torch_geometric.nn import GCNConv
-#import torch
+from torch_geometric.data import HeteroData
+import torch
 #import torch.nn.functional as F
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import itertools
 
-
 articles_df = pd.read_csv('../articles_data/full_df.csv')
+print("aarticles: ", articles_df.shape)
 articles_dict = articles_df.to_dict()
 software_df = pd.read_csv('../data/full_df.csv')
+print("software: ", software_df.shape)
 software_dict = software_df.to_dict()
-
 
 
 def keywords_vocabulary(articles_dict, software_dict):
@@ -26,20 +28,31 @@ def keywords_vocabulary(articles_dict, software_dict):
             keywords = value.split(";")
             software_keywords.append(keywords)
 
-    flattened_articles = list(itertools.chain(*articles_keywords))
-    flattened_software = list(itertools.chain(*software_keywords))
+    all_keywords = software_keywords + articles_keywords
 
-    all_keywords = flattened_articles + flattened_software
-    mlb = MultiLabelBinarizer()
+    vectorizer = TfidfVectorizer()
 
-    # Fit the binarizer to all keywords
-    mlb.fit(all_keywords)
-    software_features = mlb.transform(software_keywords)
-    articles_features = mlb.transform(articles_keywords)
+    all_features = vectorizer.fit(all_keywords)
+    software_features = vectorizer.transform(software_keywords).to_array()
+    article_features = vectorizer.transform(articles_keywords).to_array()
 
-    print("Software Features:\n", software_features)
-    print("Paper Features:\n", articles_features)
-    return articles_keywords
+    return article_features, software_features
 
 
-articles = keywords_vocabulary(articles_dict=articles_dict, software_dict=software_dict)
+def preprocess_heterodata(articles_dict, software_dict):
+    data = HeteroData()
+
+    articles_features, software_features = keywords_vocabulary(articles_dict, software_dict)
+
+    data['articles'].x = articles_features
+    data['software'].x = software_features
+
+    print(data)
+
+
+
+def build_gnn(articles_dict, software_dict):
+    articles_features, software_features = keywords_vocabulary(articles_dict=articles_dict, software_dict=software_dict)
+
+
+preprocess_heterodata(articles_dict, software_dict)
