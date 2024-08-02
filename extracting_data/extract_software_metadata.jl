a1f40c278f6ca7af40cce3fe637b4b34fe4f0d25
Mazztok45 <maxence.azzouz@gmail.com>
2024-08-02 13:04:22 +0200

    adjusting folder paths to extract software data

--- extracting_data/extract_software_metadata.jl
+++ extracting_data/extract_software_metadata.jl
@@ -9,41 +9,39 @@ This script reads the .json files from the API, creates a df from it, saves it a
 named full_df.csv
 =#
 df = DataFrame()
-json_files = readdir("../data")
+json_files = readdir("./data")
 selected_vars = [:classification, :description, :articles_count, :authors, :dependencies, :homepage,
 :id, :keywords, :license_terms, :name, :operating_systems, :orms_id,:programming_languages,
 :related_software, :source_code, :standard_articles, :zbmath_url]
 
 
 for file in json_files
-    if file == "full_df.csv"
-        continue
-    end
+    if endswith(file,".csv")==false
+        println(file)
+        json_file = JSON3.read(string("./data/", file))
+        for k in keys(json_file)
+            temp_dict = copy(json_file[k])
+            for var in selected_vars
+                # convert nothing (nan) values to strings
+                if temp_dict[var] == nothing
+                    temp_dict[var] = string()
+                end
+                # convert lists to strings with ; in between values
+                if temp_dict[var] isa Array
+                    temp_dict[var] = join(temp_dict[var], ";")
+                end
+            end
+            result = Dict{Symbol, Any}()
 
-    println(file)
-    json_file = JSON3.read(string("../data/", file))
-    for k in keys(json_file)
-        temp_dict = copy(json_file[k])
-        for var in selected_vars
-            # convert nothing (nan) values to strings
-            if temp_dict[var] == nothing
-                temp_dict[var] = string()
+            for (k,v) in temp_dict
+                if k in selected_vars
+                    push!(result, k=>v)
+                end
             end
-            # convert lists to strings with ; in between values
-            if temp_dict[var] isa Array
-                temp_dict[var] = join(temp_dict[var], ";")
-            end
+            temp_df = result |> DataFrame;
+            append!(df,temp_df)
         end
-        result = Dict{Symbol, Any}()
-
-        for (k,v) in temp_dict
-            if k in selected_vars
-                push!(result, k=>v)
-            end
-        end
-        temp_df = result |> DataFrame;
-        append!(df,temp_df)
     end
 end
 
-CSV.write("../data/full_df.csv", df)
+CSV.write("./data/full_df.csv", df)
