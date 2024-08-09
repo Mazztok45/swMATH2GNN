874a7bd83b00e0778a3bb7469dad454aa0cf1a5a
Mazztok45 <maxence.azzouz@gmail.com>
2024-08-09 21:07:16 +0200

    advancing the work on the graph

--- GNN_Julia/train_gnn.jl
+++ GNN_Julia/train_gnn.jl
@@ -6,7 +6,11 @@ using TextAnalysis
 using MultivariateStats
 using SparseArrays
 using StructTypes
-
+using TikzGraphs
+using Graphs
+using GraphPlot
+using GraphRecipes
+using Compose
 include("hetero_data.jl")
 include("../extracting_data/extract_software_metadata.jl")
 using .HeteroDataProcessing
@@ -14,6 +18,7 @@ using .DataReaderswMATH
 import .DataReaderswMATH: generate_software_dataframe
 import MultivariateStats: fit, KernelPCA
 import GraphNeuralNetworks: GNNHeteroGraph
+
 # Helper functions to handle Missing values and ensure correct types
 function convert_to_string(x)
     return !ismissing(x) ? string(x) : ""
@@ -133,12 +138,12 @@ println("Articles DataFrame columns: ", names(articles_df))
 println("Software DataFrame columns: ", names(software_df))
 
 # Create Article instances
-articles_dict = Dict{String, Article}()
-for i in 1:nrow(articles_df)
-    row = articles_df[i, :]
-    article_instance = create_article(row)
-    articles_dict[string(row[:id])] = article_instance
-end
+#articles_dict = Dict{String, Article}()
+#for i in 1:nrow(articles_df)
+#    row = articles_df[i, :]
+#    article_instance = create_article(row)
+#    articles_dict[string(row[:id])] = article_instance
+#end
 
 # Create Software instances
 #software_dict = Dict{String, Software}()
@@ -152,18 +157,41 @@ end
 # Print keys to verify dictionaries
 #println("Articles Dict Keys: ", keys(articles_dict))
 #println("Software Dict Keys: ", keys(software_dict))
-software_df.id
+
 related_soft = [collect(row)[1][2] for row in software_df.related_software]
 
 G = GNNHeteroGraph((:software,:relates_to, :software)=> (software_df.id, related_soft))
 
-# Process the data into a HeteroData() object
-try
-    data = preprocess_heterodata(G, articles_dict, software_df)
-    println("Data preprocessing successful")
-catch e
-    println("Error in preprocess_heterodata: ", e)
+##### VIZ PART ###
+filtered_df = software_df#[software_df.articles_count .> 200, :]
+related_soft_filtered_df = [collect(row)[1][2] for row in filtered_df.related_software]
+g_viz=DiGraph()
+add_vertices!(g_viz, length(Set(filtered_df.id)))
+for i in 1:nrow(filtered_df)
+    add_edge!(g_viz,filtered_df[i, :id],related_soft_filtered_df[i])
 end
+
+sg = induced_subgraph(g_viz, neighbors(g_viz,825))
+
+
+vertices_l = reduce(hcat, [[src(e), dst(e)] for e in edges(sg[1])])
+g_viz2=DiGraph()
+add_vertices!(g_viz2, length(Set(vertices_l)))
+for edge in edges(sg[1])
+    add_edge!(g_viz2, src(edge), dst(edge))
+end
+
+
+
+
+draw(PNG("soft_825_graph.png", 16cm, 16cm), gplot(g_viz2))
+# Process the data into a HeteroData() object
+#try
+#    data = preprocess_heterodata(G, articles_dict, software_df)
+#    println("Data preprocessing successful")
+#catch e
+#    println("Error in preprocess_heterodata: ", e)
+#end
 
 # Print data metadata if available
 #if isdefined(Main, :data) && data !== nothing
@@ -172,53 +200,53 @@ end
 #end
 
 # Define the transformation (RandomLinkSplit equivalent in Julia)
-function random_link_split(data; num_val, num_test, disjoint_train_ratio, neg_sampling_ratio, add_negative_train_samples, edge_types, rev_edge_types)
+##function random_link_split(data; num_val, num_test, disjoint_train_ratio, neg_sampling_ratio, add_negative_train_samples, edge_types, rev_edge_types)
     # Implement your splitting logic here
     # This is a placeholder function, implement according to your logic
-    return train_data, val_data, test_data
-end
+#    return train_data, val_data, test_data
+#end
 
 # Transform data into train, validation, and test dataset
-train_data, val_data, test_data = random_link_split(
-    data; 
-    num_val=0.1,
-    num_test=0.2,
-    disjoint_train_ratio=0.3,
-    neg_sampling_ratio=2.0,
-    add_negative_train_samples=false,
-    edge_types=("software", "mentioned_in", "article"),
-    rev_edge_types=("article", "rev_mentioned_in", "software"),
-)
+#train_data, val_data, test_data = random_link_split(
+#    data; 
+#    num_val=0.1,
+#    num_test=0.2,
+#    disjoint_train_ratio=0.3,
+#    neg_sampling_ratio=2.0,
+#    add_negative_train_samples=false,
+#    edge_types=("software", "mentioned_in", "article"),
+#    rev_edge_types=("article", "rev_mentioned_in", "software"),
+#)
 
-# Define a loader for training data (LinkNeighborLoader equivalent in Julia)
-function link_neighbor_loader(data, num_neighbors, neg_sampling_ratio, edge_label_index, edge_label, batch_size, shuffle)
+# Define a loader for training data (LinkNeighborLoader equivalent in Julia)#
+#function link_neighbor_loader(data, num_neighbors, neg_sampling_ratio, edge_label_index, edge_label, batch_size, shuffle)
     # Implement your loader logic here
     # This is a placeholder function, implement according to your logic
-    return loader
-end
+#    return loader
+#end
 
-edge_label_index = train_data["software", "mentioned_in", "article"].edge_label_index
-edge_label = train_data["software", "mentioned_in", "article"].edge_label
+#edge_label_index = train_data["software", "mentioned_in", "article"].edge_label_index
+#edge_label = train_data["software", "mentioned_in", "article"].edge_label
 
-train_loader = link_neighbor_loader(
-    data=train_data,
-    num_neighbors=[20, 10],
-    neg_sampling_ratio=2.0,
-    edge_label_index=(("software", "mentioned_in", "article"), edge_label_index),
-    edge_label=edge_label,
-    batch_size=128,
-    shuffle=true,
-)
+#train_loader = link_neighbor_loader(
+#    data=train_data,
+#    num_neighbors=[20, 10],
+#    neg_sampling_ratio=2.0,
+#    edge_label_index=(("software", "mentioned_in", "article"), edge_label_index),
+#    edge_label=edge_label,
+#    batch_size=128,
+#    shuffle=true,
+#)
 
 # Sample a mini-batch
-function get_sampled_data(loader)
+#function get_sampled_data(loader)
     # Implement your sampling logic here
     # This is a placeholder function, implement according to your logic
-    return sampled_data
-end
+#    return sampled_data
+#end
 
-sampled_data = get_sampled_data(train_loader)
+#sampled_data = get_sampled_data(train_loader)
 
-println("Sampled mini-batch:")
-println("===================")
+#println("Sampled mini-batch:")
+#println("===================")
 #println(sampled_data)
