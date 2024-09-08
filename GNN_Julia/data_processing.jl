f20293ad6d122d65560e652eea81e8a334b09c5a
Azzouz-Thuderoz <maxence.azzouz@gmail,com>
2024-09-08 19:56:12 +0200

    major update, splitting the data processing and the training part

---
+++ GNN_Julia/data_processing.jl
@@ -0,0 +1,121 @@
+using CSV
+using DataFrames
+using GraphNeuralNetworks
+using Flux
+#using TextAnalysis
+#using MultivariateStats
+using SparseArrays
+using StructTypes
+using Graphs
+using Combinatorics
+#include("hetero_data.jl")
+include("../extracting_data/extract_software_metadata.jl")
+include("../extracting_data/extract_article_metadata.jl")
+#using .HeteroDataProcessing
+using .DataReaderswMATH
+import .DataReaderswMATH: generate_software_dataframe
+import .DataReaderszbMATH: extract_articles_metadata
+#import MultivariateStats: fit, KernelPCA
+import GraphNeuralNetworks: GNNHeteroGraph, GNNGraphs
+using OneHotArrays
+using MLLabelUtils
+using StatsBase
+
+
+########### FUNCTIONS THAT MUST BE KEPT ###########
+#articles_list_dict = extract_articles_metadata()
+
+#node_features = [dic[:msc] for dic in articles_list_dict]
+#art_soft = [dic[:software] for dic in articles_list_dict]
+###
+
+
+### Function to write articles MSC
+#msc_vec = [join(elem,";") for elem in node_features]
+#msc_str = join(msc_vec,'\n')
+#open("msc.txt", "w") do file
+#    write(file, msc_str)
+#end
+###
+
+### Function to write articles articles software labels
+#art_soft_str = join(art_soft,'\n')
+#open("art_soft.txt", "w") do file
+#    write(file, art_soft_str)
+#end
+########### END ###########
+
+
+##### Articles MSC codes data processing
+### Function to read articles msc
+node_features = map(x -> split(x,";"), split(read("msc.txt",String), '\n'))
+#### Remove empty MSC codes
+filt_empt_nf =[filter!(x -> !isempty(x), y) for y in node_features if isempty(y)==false]
+### Extending node_features with parent MSC codes
+new_nodes_features= Vector{Vector}()
+maj_cat_vector = Vector{Vector}()
+for l in filt_empt_nf
+    maj_codes=unique([str_msc[1:2] for str_msc in l])
+    push!(new_nodes_features, vcat(l,maj_codes))
+    push!(maj_cat_vector, maj_codes)
+end
+
+### Unique set of MSC codes
+cat_nodes = filter!(!isempty,unique(reduce(vcat, new_nodes_features)))
+### Unique set of parent MSC codes
+maj_cat = filter!(!isempty,unique(reduce(vcat, maj_cat_vector)))
+
+
+###Encoding the nodes
+enc=labelenc(cat_nodes)
+cat_nodes_enc=map(x -> enc.invlabel[x],cat_nodes)
+maj_cat_enc=map(x -> enc.invlabel[x],map(x -> x[1:2],cat_nodes))
+DataFrame(col1=maj_cat_enc,col2=cat_nodes_enc)
+
+CSV.write("msc_edges.csv", DataFrame(col1=maj_cat_enc,col2=cat_nodes_enc))
+
+
+### Get the encoded MSC node features
+enc_node_features=[map(x -> enc.invlabel[x], y) for y in new_nodes_features]
+
+df = DataFrame(msc=enc_node_features)
+ux = unique(reduce(vcat, df.msc))
+#### One hot array
+df_enc = transform(df, :msc .=> [ByRow(v -> x in v) for x in ux] .=> Symbol.(:msc_, ux))
+### Changing the types
+for x in names(df_enc)[2:5509]
+    df_enc[!,x] = convert.(Int,df_enc[!,x])
+end
+
+CSV.write("msc_encoding_mat.csv", df_enc[:,2:5509])
+
+
+
+
+
+##### Software data processing
+## Reading data
+software_df  = generate_software_dataframe()
+art_soft = split(read("art_soft.txt",String), '\n')
+
+### Encoding software
+enc_soft=labelenc(art_soft)
+soft_enc=map(x -> enc_soft.invlabel[x],art_soft)
+
+
+soft_enc_str = join(soft_enc,'\n')
+open("soft_enc.csv", "w") do file
+    write(file, soft_enc_str)
+end
+
+
+
+u_soft = map(x->parse(Int64,x),unique(art_soft))
+
+related_soft = [collect(row)[1][2] for row in software_df.related_software]
+
+df_rel_soft=DataFrame(col1=software_df.id, col2=related_soft)
+filt_rel_soft = filter(row -> row.col1 in u_soft && row.col2 in u_soft, df_rel_soft)
+
+CSV.write("filt_rel_soft.csv", filt_rel_soft )
+
--- GNN_Julia/train_gnn.jl
+++ GNN_Julia/train_gnn.jl
@@ -16,117 +16,36 @@ using .DataReaderswMATH
 import .DataReaderswMATH: generate_software_dataframe
 import .DataReaderszbMATH: extract_articles_metadata
 #import MultivariateStats: fit, KernelPCA
-import GraphNeuralNetworks: GNNHeteroGraph
+import GraphNeuralNetworks: GNNHeteroGraph, GNNGraphs
+using OneHotArrays
+using MLLabelUtils
+using StatsBase
 
-software_df  = generate_software_dataframe()
-
-#### FUNCTIONS THAT MUST BE KEPT
-#articles_list_dict = extract_articles_metadata()
-#node_features = [dic[:msc] for dic in articles_list_dict]
-###
-
-
-### Function to write articles MSC
-msc_vec = [join(elem,";") for elem in node_features]
-msc_str = join(msc_vec,'\n')
-open("msc.txt", "w") do file
-    write(file, msc_str)
+##### GRAPH PREP
+function msc_encoding()
+    return Matrix(DataFrame(CSV.File("msc_encoding_mat.csv")))
 end
-###
-
-### Function to read articles msc
-node_features = map(x -> split(x,";"), split(read("msc.txt",String), '\n'))
-###
-
-####
-collect(combinations(node_features[3], 2))
-
-msc_df = CSV.read("MSC_2020(5).csv",DataFrame)
-# Print the column names of the DataFrames to verify
-#println("Articles DataFrame columns: ", names(articles_df))
-println("Software DataFrame columns: ", names(software_df))
-
-u_soft = unique([dic[:software] for dic in articles_list_dict])
-
-related_soft = [collect(row)[1][2] for row in software_df.related_software]
-
-G = GNNHeteroGraph((:software,:relates_to, :software)=> (software_df.id, related_soft))
-
-using PaddedViews  # for padding functionality
-
-# Determine the maximum size of any array
 
 
+## Two columns DataFrame of related each other software
+filt_rel_soft=DataFrame(CSV.File("filt_rel_soft.csv"))
 
-pad_mat = (x=unique(node_features) .== permutedims(node_features))
+u_soft = size(unique(vcat(filt_rel_soft.col1,filt_rel_soft.col2)))[1]  ## 32807
 
-GNNHeteroGraph(G, ndata = pad_mat)
+maj_cat_enc=DataFrame(CSV.File("msc_edges.csv")).col1
+cat_nodes_enc=DataFrame(CSV.File("msc_edges.csv")).col2
+
+data = ((:software,:relates_to, :software)=> (filt_rel_soft.col1, filt_rel_soft.col2),
+(:msc,:parent_cat,:msc)=>(maj_cat_enc, cat_nodes_enc))
+
+num_nodes = Dict(:msc => 5571, :software => size(unique(vcat(filt_rel_soft.col1,filt_rel_soft.col2)))[1]
+)
+
+soft_enc = map(x->parse(Int64,x),split(read("soft_enc.csv",String), '\n'))
 
 
+soft_mat = permutedims(unique(soft_enc) .== permutedims(soft_enc))
 
+ndata=Dict(:msc => (x = msc_encoding()),:software =>(y=soft_mat))
 
-# Process the data into a HeteroData() object
-#try
-#    data = preprocess_heterodata(G, articles_dict, software_df)
-#    println("Data preprocessing successful")
-#catch e
-#    println("Error in preprocess_heterodata: ", e)
-#end
-
-# Print data metadata if available
-#if isdefined(Main, :data) && data !== nothing
-#    metadata = data.metadata
-#    println(metadata)
-#end
-
-# Define the transformation (RandomLinkSplit equivalent in Julia)
-##function random_link_split(data; num_val, num_test, disjoint_train_ratio, neg_sampling_ratio, add_negative_train_samples, edge_types, rev_edge_types)
-    # Implement your splitting logic here
-    # This is a placeholder function, implement according to your logic
-#    return train_data, val_data, test_data
-#end
-
-# Transform data into train, validation, and test dataset
-#train_data, val_data, test_data = random_link_split(
-#    data; 
-#    num_val=0.1,
-#    num_test=0.2,
-#    disjoint_train_ratio=0.3,
-#    neg_sampling_ratio=2.0,
-#    add_negative_train_samples=false,
-#    edge_types=("software", "mentioned_in", "article"),
-#    rev_edge_types=("article", "rev_mentioned_in", "software"),
-#)
-
-# Define a loader for training data (LinkNeighborLoader equivalent in Julia)#
-#function link_neighbor_loader(data, num_neighbors, neg_sampling_ratio, edge_label_index, edge_label, batch_size, shuffle)
-    # Implement your loader logic here
-    # This is a placeholder function, implement according to your logic
-#    return loader
-#end
-
-#edge_label_index = train_data["software", "mentioned_in", "article"].edge_label_index
-#edge_label = train_data["software", "mentioned_in", "article"].edge_label
-
-#train_loader = link_neighbor_loader(
-#    data=train_data,
-#    num_neighbors=[20, 10],
-#    neg_sampling_ratio=2.0,
-#    edge_label_index=(("software", "mentioned_in", "article"), edge_label_index),
-#    edge_label=edge_label,
-#    batch_size=128,
-#    shuffle=true,
-#)
-
-# Sample a mini-batch
-#function get_sampled_data(loader)
-    # Implement your sampling logic here
-    # This is a placeholder function, implement according to your logic
-#    return sampled_data
-#end
-
-#sampled_data = get_sampled_data(train_loader)
-
-#println("Sampled mini-batch:")
-#println("===================")
-#println(sampled_data)
+G = GNNHeteroGraph(data; num_nodes, ndata)
