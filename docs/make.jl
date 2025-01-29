36ed2dc81fd182380a5fb0635eab4f4f46a7e627
Mazztok45 <maxence.azzouz@gmail.com>
2025-01-29 17:11:22 +0100

    initalizing Documenter + cleaning other old files

---
+++ docs/make.jl
@@ -0,0 +1,4 @@
+using Documenter
+using swMATH2GNN
+
+makedocs(sitename = "swMATH2GNN.jl")
---
+++ src/swMATH2GNN.jl
@@ -0,0 +1,9 @@
+module swMATH2GNN
+
+export software_pipeline
+
+function software_pipeline()
+    return "pipeline is working"
+end
+
+end # module
