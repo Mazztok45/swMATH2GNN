4f7315763a8be12e1ddd2ccb4700dae3c264f494
Mazztok45 <maxence.azzouz@gmail.com>
2025-05-21 19:21:47 +0200

    refactoring code

--- src/GNN_Julia/text_modeling.jl
+++ src/GNN_Julia/text_modeling.jl
@@ -271,48 +271,7 @@ filtered_df = filter(row -> row.ArticleID in ids, filtered_df  )
 # The `new_eval_mask` ensures only unique identifiers are marked with 1.
 
 #############
-#Graph
 
-dfs=unique(software_df[:,[:id,:classification]])
-# Create a new column for classifications as an array of strings
-dfs.classification_split = [split(x, ";") for x in dfs.classification]
- # Flatten the list of classifications and convert it into a DataFrame
- classification_list = vcat(dfs.classification_split...)
- # Flatten the list of classifications and convert it into a DataFrame
- classification_counts = DataFrame(classification=classification_list)
-# Count the occurrences of each classification
-classification_summary = combine(groupby(classification_counts, :classification), nrow => :count)
-
-
-# Sort the DataFrame by classification in ascending order
-
-sorted_classification_summary = sort(classification_summary, :classification)
-
-# Extract data from the sorted DataFrame
-classifications = sorted_classification_summary.classification
-counts = sorted_classification_summary.count
-
-# Determine appropriate y-tick values
-yticks_values = 0:2000:maximum(counts)
-
-# Plot a bar chart of classification counts
-bar_plot = bar(classifications, counts, 
-    xlabel = "MSC Code", 
-    ylabel = "Software count", 
-    title = "Distribution of Software Count per MSC Code", 
-    legend = false, 
-    xtickfont = font(8), 
-    rotation = 45,
-    yticks = (yticks_values, [string(v) for v in yticks_values]))  # Set y-ticks explicitly with readable labels
-
-# Display the plot
-
-# Save the plot to a file (e.g., PNG)
-savefig(bar_plot, "msc_code_distribution.png")
-
-# You can also use other formats like:
-# savefig(bar_plot, "msc_code_distribution.pdf")
-# savefig(bar_plot, "msc_code_distribution.svg")
 
 #############
 
--- src/graph_viz_julia/msc_distribution.jl
+++ src/graph_viz_julia/msc_distribution.jl
@@ -1,17 +1,56 @@
+if basename(pwd()) != "swMATH2GNN"
+    cd("..")
+end 
+
 using DataFrames
 using StatsBase
 using Plots
-
-using .HeteroDataProcessing
+include("../extracting_data/extract_software_metadata.jl")
+#using .HeteroDataProcessing
 using .DataReaderswMATH
 import .DataReaderswMATH: generate_software_dataframe
 
 software_df  = generate_software_dataframe()
 
-### MSC codes
-msc = unique(software_df[!,[:id, :classification]]).classification
+#Graph
 
-l = reduce(vcat, [split(elem, ";") for elem in msc])
+dfs=unique(software_df[:,[:id,:classification]])
+# Create a new column for classifications as an array of strings
+dfs.classification_split = [split(x, ";") for x in dfs.classification]
+ # Flatten the list of classifications and convert it into a DataFrame
+ classification_list = vcat(dfs.classification_split...)
+ # Flatten the list of classifications and convert it into a DataFrame
+ classification_counts = DataFrame(classification=classification_list)
+# Count the occurrences of each classification
+classification_summary = combine(groupby(classification_counts, :classification), nrow => :count)
 
-countmap(l)
-plot(countmap(l))
+
+# Sort the DataFrame by classification in ascending order
+
+sorted_classification_summary = sort(classification_summary, :classification)
+
+# Extract data from the sorted DataFrame
+classifications = sorted_classification_summary.classification
+counts = sorted_classification_summary.count
+
+# Determine appropriate y-tick values
+yticks_values = 0:2000:maximum(counts)
+
+# Plot a bar chart of classification counts
+bar_plot = bar(classifications, counts, 
+    xlabel = "MSC Code", 
+    ylabel = "Software count", 
+    title = "Distribution of Software Count per MSC Code", 
+    legend = false, 
+    xtickfont = font(8), 
+    rotation = 45,
+    yticks = (yticks_values, [string(v) for v in yticks_values]))  # Set y-ticks explicitly with readable labels
+
+# Display the plot
+
+# Save the plot to a file (e.g., PNG)
+savefig(bar_plot, "msc_code_distribution.png")
+
+# You can also use other formats like:
+# savefig(bar_plot, "msc_code_distribution.pdf")
+# savefig(bar_plot, "msc_code_distribution.svg")
